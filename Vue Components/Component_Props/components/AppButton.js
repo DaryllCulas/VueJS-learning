73594e040b67bfda3d2436d1f37029911f8d5ac9
DaryllCulas <culasdaryll99@gmail.com>
2024-08-22 02:18:21 +0800

    Done

--- Vue Components/Component_Props/components/AppButton.js
+++ Vue Components/Component_Props/components/AppButton.js
@@ -1,11 +1,24 @@
 export default {
 
-    template: `<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded px-5 py-2 disabled:cursor-not-allowed" v-bind:disabled="processing">
+    template: `<button 
+    v-bind:class="{
+      'border rounded px-5 py-2 disabled:cursor-not-allowed':true,
+      'bg-blue-200 hover:bg-blue-400' : type === 'primary',
+      'bg-orange-200 hover:bg-orange-400' : type === 'secondary',
+      'bg-gray-200 hover:bg-gray-400' : type === 'muted',
+      'is-loading': processing
+    }" 
+    v-bind:disabled="processing">
     <slot />
     </button>`,
-  data() {
-    return {
-      processing: true,
-    };
-  },
+    props: {
+      type: {
+        type: String,
+        default: "primary",
+      },
+      processing: {
+        type: Boolean,
+        default: false
+       }
+    },
 }
