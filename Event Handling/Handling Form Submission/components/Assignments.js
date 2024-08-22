6ac3722b9c8dba353cd3b8e150cd652871056799
DaryllCulas <culasdaryll99@gmail.com>
2024-08-23 06:08:47 +0800

    done

--- Event Handling/Handling Form Submission/components/Assignments.js
+++ Event Handling/Handling Form Submission/components/Assignments.js
@@ -3,10 +3,18 @@ import AssignmentList from "./AssignmentList.js";
 export default {
     components: { AssignmentList },
     template: `
-    <section class="space-y-6">
+    <section class="space-y-5">
         <AssignmentList v-bind:assignments="filters.inProgress" title="In progress"/>
         <AssignmentList v-bind:assignments="filters.completed" title="Completed"/>
+
+    <form @submit.prevent="add">
+    <div class="border border-gray-600 text-black">
+      <input v-model="newAssignment" placeholder="New Assignment..." class="p-2" />
+      <button type="submit" class="bg-white p-2 border-l">Add</button>
+      </div>
+    </form>
     </section>
+   
    
     `,
     data() {
@@ -14,8 +22,9 @@ export default {
           assignments: [
             { name: "Finish project", completed: false, id: 1 },
             { name: "Read Chapter 1", completed: false, id: 2 },
-            { name: "Dota", completed: false, id: 3 },
+            { name: "Playing Dota", completed: false, id: 3 },
           ],
+          newAssignment: "",
         };
       },
       computed: {
@@ -27,7 +36,19 @@ export default {
                 completed: this.assignments.filter(
                     (assignments) => assignments.complete
                   ),
-            }
+            };
         }
       },
+
+      methods: {
+        add() {
+          this.assignments.push({
+            name: this.newAssignment,
+            completed: false,
+            id: this.assignments.length + 1,
+          });
+          this.newAssignment = "";
+          
+        }
+      }
 }
