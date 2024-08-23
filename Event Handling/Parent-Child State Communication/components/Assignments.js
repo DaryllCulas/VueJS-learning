79bf578bd8caf7e9059d3161805bd52b310a4fdd
DaryllCulas <culasdaryll99@gmail.com>
2024-08-24 01:48:26 +0800

    Done

---
+++ Event Handling/Parent-Child State Communication/components/Assignments.js
@@ -0,0 +1,47 @@
+import AssignmentList from './AssignmentList.js'
+import AssignmentCreate from './AssignmentCreate.js'
+
+export default {
+  components: { AssignmentList, AssignmentCreate },
+  template: `
+    <section class="space-y-5">
+        <AssignmentList v-bind:assignments="filters.inProgress" title="In progress"/>
+        <AssignmentList v-bind:assignments="filters.completed" title="Completed"/>
+        <AssignmentCreate v-on:add="add"></AssignmentCreate>
+ 
+    </section>
+   
+   
+    `,
+  data () {
+    return {
+      assignments: [
+        { name: 'Finish project', completed: false, id: 1 },
+        { name: 'Read Chapter 1', completed: false, id: 2 },
+        { name: 'Playing Dota', completed: false, id: 3 }
+      ],
+     
+    }
+  },
+  computed: {
+    filters () {
+      return {
+        inProgress: this.assignments.filter(
+          assignments => !assignments.complete
+        ),
+        completed: this.assignments.filter(assignments => assignments.complete)
+      }
+    }
+  },
+
+  methods: {
+    add (name) {
+      this.assignments.push({
+        name: name,
+        completed: false,
+        id: this.assignments.length + 1
+      })
+     
+    }
+  }
+}
