d63f8c57e3f68e41db96fd418da60b3e98483964
DaryllCulas <culasdaryll99@gmail.com>
2024-08-24 01:48:01 +0800

    Done

--- Event Handling/Handling Form Submission/components/Assignments.js
+++ Event Handling/Handling Form Submission/components/Assignments.js
@@ -1,8 +1,8 @@
-import AssignmentList from "./AssignmentList.js";
+import AssignmentList from './AssignmentList.js'
 
 export default {
-    components: { AssignmentList },
-    template: `
+  components: { AssignmentList },
+  template: `
     <section class="space-y-5">
         <AssignmentList v-bind:assignments="filters.inProgress" title="In progress"/>
         <AssignmentList v-bind:assignments="filters.completed" title="Completed"/>
@@ -17,38 +17,35 @@ export default {
    
    
     `,
-    data() {
-        return {
-          assignments: [
-            { name: "Finish project", completed: false, id: 1 },
-            { name: "Read Chapter 1", completed: false, id: 2 },
-            { name: "Playing Dota", completed: false, id: 3 },
-          ],
-          newAssignment: "",
-        };
-      },
-      computed: {
-        filters() {
-            return {
-                inProgress: this.assignments.filter(
-                    (assignments) => !assignments.complete
-                  ),
-                completed: this.assignments.filter(
-                    (assignments) => assignments.complete
-                  ),
-            };
-        }
-      },
-
-      methods: {
-        add() {
-          this.assignments.push({
-            name: this.newAssignment,
-            completed: false,
-            id: this.assignments.length + 1,
-          });
-          this.newAssignment = "";
-          
-        }
+  data () {
+    return {
+      assignments: [
+        { name: 'Finish project', completed: false, id: 1 },
+        { name: 'Read Chapter 1', completed: false, id: 2 },
+        { name: 'Playing Dota', completed: false, id: 3 }
+      ],
+      newAssignment: ''
+    }
+  },
+  computed: {
+    filters () {
+      return {
+        inProgress: this.assignments.filter(
+          assignments => !assignments.complete
+        ),
+        completed: this.assignments.filter(assignments => assignments.complete)
       }
+    }
+  },
+
+  methods: {
+    add () {
+      this.assignments.push({
+        name: this.newAssignment,
+        completed: false,
+        id: this.assignments.length + 1
+      })
+      this.newAssignment = ''
+    }
+  }
 }
