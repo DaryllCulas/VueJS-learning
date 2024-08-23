d82b8e7f732eace76004ad592f45775d93ee045e
DaryllCulas <culasdaryll99@gmail.com>
2024-08-24 03:08:02 +0800

    Component Responsibility Added

---
+++ Beyond the Basics/Component Responsibility/components/AssignmentList.js
@@ -0,0 +1,55 @@
+import AssignmentListItem from "./AssignmentListItem.js";
+
+export default {
+    components: { AssignmentListItem },
+    template: `
+      <section v-show="assignments.length">
+            <h2 class="font-bold mb-2">
+            {{ title }}
+             <span>({{ assignments.length }})</span>
+             </h2>
+
+             <div class="flex gap-2">
+                <button 
+                    @click = "currentTag = tag"
+                    v-for="tag in tags" 
+                    class="border rounded px-1 py-px text-xs"
+                    v-bind:class="{
+                        'border-blue-500 text-blue-500': tag === currentTag
+                    
+                    }"
+                    >
+                    {{ tag }}
+                 </button>
+             </div>
+    
+            <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
+             <AssignmentListItem 
+                v-for="assignment in filteredAssignments" 
+                v-bind:key="assignment.id"
+                v-bind:assignment="assignment"
+                ></AssignmentListItem>
+            </ul>
+          </section>
+    `,
+    props: {
+        assignments: Array,
+        title: String    
+    },
+    data() {
+        return {
+            currentTag: 'all'
+        };
+    },
+    computed: {
+        filteredAssignments() {
+            if(this.currentTag === 'all') {
+                return this.assignments;
+            }
+            return this.assignments.filter(a => a.tag === this.currentTag);
+        },
+        tags() {
+            return['all',...new Set(this.assignments.map(a => a.tag))];
+        }
+    }
+}
