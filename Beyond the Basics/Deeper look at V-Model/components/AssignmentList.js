3a64f36afe4976bc8f4daa45cdb42ed4010d11f6
DaryllCulas <culasdaryll99@gmail.com>
2024-08-24 04:30:13 +0800

    Deeper look at V-model added

---
+++ Beyond the Basics/Deeper look at V-Model/components/AssignmentList.js
@@ -0,0 +1,46 @@
+import AssignmentListItem from "./AssignmentListItem.js";
+import AssignmentTags from "./AssignmentTags.js";
+
+export default {
+    components: { AssignmentListItem, AssignmentTags },
+    template: `
+      <section v-show="assignments.length">
+            <h2 class="font-bold mb-2">
+            {{ title }}
+             <span>({{ assignments.length }})</span>
+             </h2>
+
+          <AssignmentTags 
+          :initial-tags="assignments.map(a => a.tag)"
+          :current-tag="currentTag"
+          @change="currentTag = $event"
+           />
+    
+            <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
+             <AssignmentListItem 
+                v-for="assignment in filteredAssignments" 
+                v-bind:key="assignment.id"
+                v-bind:assignment="assignment"
+                ></AssignmentListItem>
+            </ul>
+          </section>
+    `,
+    props: {
+        assignments: Array,
+        title: String    
+    },
+    data() {
+        return {
+            currentTag: 'all'
+        };
+    },
+    computed: {
+        filteredAssignments() {
+            if(this.currentTag === 'all') {
+                return this.assignments;
+            }
+            return this.assignments.filter(a => a.tag === this.currentTag);
+        },
+       
+    }
+}
