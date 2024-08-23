1f7d5b08304d428731b678d720d9c22503a6528a
DaryllCulas <culasdaryll99@gmail.com>
2024-08-24 01:47:27 +0800

    Beyond the basics added

---
+++ Beyond the Basics/Parent-Child State Communication/components/AssignmentList.js
@@ -0,0 +1,25 @@
+import AssignmentListItem from "./AssignmentListItem.js";
+
+export default {
+    components: { AssignmentListItem },
+    template: `
+      <section v-show="assignments.length">
+            <h2 class="font-bold mb-2">
+            {{ title }}
+             <span>({{ assignments.length }})</span>
+             </h2>
+    
+            <ul class="border border-gray-600 divide-y divide-gray-600">
+             <AssignmentListItem 
+                v-for="assignment in assignments" 
+                v-bind:key="assignment.id"
+                v-bind:assignment="assignment"
+                ></AssignmentListItem>
+            </ul>
+          </section>
+    `,
+    props: {
+        assignments: Array,
+        title: String    
+    }
+}
