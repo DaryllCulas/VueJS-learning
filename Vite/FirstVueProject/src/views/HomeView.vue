3a3eec0b4282beb784fa51f7195dc927bbcb41a2
DaryllCulas <culasdaryll99@gmail.com>
2024-08-27 05:12:27 +0800

    Modified

--- Vite/FirstVueProject/src/views/HomeView.vue
+++ Vite/FirstVueProject/src/views/HomeView.vue
@@ -1,9 +1,20 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+
+// eslint-disable-next-line no-undef
+let message = $ref('Hello World')
+
+setTimeout(() => {
+  message = 'I have been changed'
+}, 2000)
 </script>
 
 <template>
   <main>
     <TheWelcome />
+
+    <p>{{ message }}</p>
+
+    <input type="text" v-model="message" />
   </main>
 </template>
--- Vite/FirstVueProject/vite.config.js
+++ Vite/FirstVueProject/vite.config.js
@@ -7,8 +7,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    vueDevTools(),
+    vue({
+      reactivityTransform: true
+    }),
+    vueDevTools()
   ],
   resolve: {
     alias: {
