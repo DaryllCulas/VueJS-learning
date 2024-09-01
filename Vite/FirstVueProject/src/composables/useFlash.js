c433541b15dd943c8e7ee4f50c734975caee3534
DaryllCulas <culasdaryll99@gmail.com>
2024-09-01 15:53:59 +0800

    Removed

--- Vite/FirstVueProject/src/composables/useFlash.js
+++
@@ -1,9 +0,0 @@
-import swal from 'sweetalert'
-
-export function useFlash() {
-  function flash(alertMessage, message) {
-    return swal(alertMessage, message, 'success')
-  }
-
-  return { flash }
-}
