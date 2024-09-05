cc1628b58c218a4857ddbdcdacc4e2ca936d0552
DaryllCulas <culasdaryll99@gmail.com>
2024-09-06 02:54:59 +0800

    defineProps

--- Vite/VueJobs/src/App.vue
+++ Vite/VueJobs/src/App.vue
@@ -1,5 +1,10 @@
+<script setup>
+import NavBar from '@/components/NavBar.vue';
+import Hero from '@/components/Hero.vue'
 
+</script>
 <template>
-  <h1 class="text-2xl text-red-600">Hello Vue Jobs</h1>
+  <NavBar />
+  <Hero />
 </template>
 
