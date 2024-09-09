f9edc173fc5eaf7fe235e88bcb9dd7872530c438
DaryllCulas <culasdaryll99@gmail.com>
2024-09-09 23:31:53 +0800

    Toast Notification

--- Vite/VueJobs/src/main.js
+++ Vite/VueJobs/src/main.js
@@ -1,5 +1,7 @@
 import './assets/main.css';
 import 'primeicons/primeicons.css';
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 import App from './App.vue'
 import router from './router';
 import { createApp } from 'vue'
@@ -7,4 +9,5 @@ import { createApp } from 'vue'
 
 const app = createApp(App)
 app.use(router);
+app.use(Toast);
 app.mount('#app')
